2001852ae6d0777180238b967914d83a551ef6c2
Mehrshad Firouzian <mrfirouz007@gmail.com>
2023-10-31 11:44:29 +0330

    receive data from rpi socket and send them to gui middleware

--- src/host/joystick_capture.py
+++ src/host/joystick_capture.py
@@ -8,13 +8,23 @@ import pygame
 import socket
 import time
 import json
+from threading import Thread
 
-rpi_ip ='192.168.171.38'
+from monitor import Monitor
+
+
+rpi_ip ='192.168.66.38'
 rpi_port = 12345
 
-# udp setting
+# udp client setting
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 rpi_address = (rpi_ip, rpi_port)
+
+monitor = Monitor()
+
+# udp server settings
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', 9000))
 
 pygame.init()
 
@@ -22,26 +32,41 @@ pygame.init()
 controller = pygame.joystick.Joystick(0)
 controller.init()
 
+
 def pretty(i):
     data = controller.get_axis(i)
     return float(f'{data:.3f}')
 
+def send_loop():
+    while event := pygame.event.poll():
+        if event.type == pygame.JOYAXISMOTION or event.type == pygame.JOYBUTTONDOWN:
+            data = {
+                "buttons": [controller.get_button(i) for i in range(controller.get_numbuttons())],
+                "hats": controller.get_hat(0),
+                "left_joystick": (pretty(0), pretty(1)),
+                "right_joystick":(pretty(3), pretty(4)),
+                "thrust_left": pretty(2), # -1 to 1
+                "thrust_right": pretty(5), # -1 to 1
+            }
+            data_string = json.dumps(data)
+            print(data_string)
+            sock.sendto(data_string.encode(), rpi_address)
+            time.sleep(0.1)
+
+
+sock.settimeout(0.05)
+t = Thread(target=send_loop)
 try:
     while True:
-        while event := pygame.event.poll():
-            if event.type == pygame.JOYAXISMOTION or event.type == pygame.JOYBUTTONDOWN:
-                data = {
-                    "buttons": [controller.get_button(i) for i in range(controller.get_numbuttons())],
-                    "hats": controller.get_hat(0),
-                    "left_joystick": (pretty(0), pretty(1)),
-                    "right_joystick":(pretty(3), pretty(4)),
-                    "thrust_left": pretty(2), # -1 to 1
-                    "thrust_right": pretty(5), # -1 to 1
-                }
-                data_string = json.dumps(data)
-                print(data_string)
-                sock.sendto(data_string.encode(), rpi_address)
-                time.sleep(0.1)
+        if not t.is_alive():
+            t = Thread(target=send_loop)
+            t.start()
+        try:
+            data, addr = sock.recvfrom(1024)
+            if data:
+                monitor.update_json(data.decode())
+        except TimeoutError as toe:
+            continue
 
 except KeyboardInterrupt:
     controller.quit()
